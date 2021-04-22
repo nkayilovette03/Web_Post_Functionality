fa93fba915129b257be76a84786fdc636904e7d6
Nkayi Lovette <nkayilovette03@gmail.com>
2021-04-22 02:54:59 +0100

    I tried to deploy the API on heroku but it had some issues like it deployed successfully but did not open the app screen

--- server/index.js
+++ server/index.js
@@ -15,8 +15,12 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello Welcome to memories API...");
+});
+
 // const CONNECTION_URL =
-//   "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+//   "mongodb+srv://nkayilovette-memories-db:nkayilovette-memories-db@cluster0.lmsx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
